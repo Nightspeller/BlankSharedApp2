23317188705d88d87423a9172c27377565f52ba3
Serg Nights <serj.nights@live.com>
2015-06-14 19:35:37 -0700

    Initial commit

---
+++ BlankSharedApp2/BlankSharedApp2.Shared/js/config.js
@@ -0,0 +1,9 @@
+﻿var appConfig = (function () {
+    var appName = 'streamViewer';
+    var dependancies = ['ngWebSocket'];
+
+    return {
+        appName: appName,
+        dependancies: dependancies
+    }
+})();
